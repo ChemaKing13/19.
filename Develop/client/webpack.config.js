f70b6eb3e76d587c10e3c434ecf59d6b96296f39
ChemaKing13 <jm2000ho@gmail.com>
2023-07-23 20:48:19 -0600

    webpack.config.js updated

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -8,17 +8,49 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
+     
     mode: 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generates an HTML based on the specified template
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      // Injects the customer service worker file into the build
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Creates a manifest.json file, provides essential metadata for our PWD
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Take notes with JavaScript syntax hightlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        orientation: 'portrait',
+        display: 'standalone',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
@@ -31,6 +63,7 @@ module.exports = () => {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
+            // babel configuration
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
